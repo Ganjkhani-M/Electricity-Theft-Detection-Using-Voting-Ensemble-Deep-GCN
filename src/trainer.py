e27891e83243899a9ac57b1006d8f5aeff1f3fb0
Mohammad Ganjkhani <m.ganjkhani1995@gmail.com>
2026-09-07 12:37:29 +0330

    Log training device and residual connection info
    Added a print statement to indicate training device and residual connection usage.

--- src/trainer.py
+++ src/trainer.py
@@ -23,6 +23,7 @@ def train_gcn(x, y, train_mask, val_mask, edge_index, hidden=32, embed_dim=32, l
                 data = data.to(device)
 
                 model = UserGCN(in_channels = x.shape[1], hidden = hidden, out_embed = embed_dim, num_layers = num_layers, use_residual = use_residual).to(device)
+                print(f'Training GCN on {device}. Residual Connections: {use_residual}')
 
                 # Positive class weighting to deal with data imbalance
                 pos_weight = torch.tensor([
