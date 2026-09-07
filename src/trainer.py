87da07fbb72ae0e9900acc2fea9b05a4f79c5c04
Mohammad Ganjkhani <m.ganjkhani1995@gmail.com>
2026-09-07 12:22:26 +0330

    Add num_layers parameter to UserGCN model

--- src/trainer.py
+++ src/trainer.py
@@ -22,7 +22,7 @@ def train_gcn(x, y, train_mask, val_mask, edge_index, hidden=32, embed_dim=32, l
                 data.val_mask = val_mask
                 data = data.to(device)
 
-                model = UserGCN(in_channels = x.shape[1], hidden = hidden, out_embed = embed_dim).to(device)
+                model = UserGCN(in_channels = x.shape[1], hidden = hidden, out_embed = embed_dim, num_layers = config.GCN_layers).to(device)
 
                 # Positive class weighting to deal with data imbalance
                 pos_weight = torch.tensor([
